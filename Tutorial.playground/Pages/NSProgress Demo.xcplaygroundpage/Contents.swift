cc5421eee2d575bcea09fae879c7024f61f0c07c
Andreas Grosam <agrosam@onlinehome.de>
2016-03-22 10:53:30 +0100

    Fix Playgrounds NSProgress Demo

--- Tutorial.playground/Pages/NSProgress Demo.xcplaygroundpage/Contents.swift
+++ Tutorial.playground/Pages/NSProgress Demo.xcplaygroundpage/Contents.swift
@@ -10,15 +10,12 @@ XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 //: # NSProgress Demo
 //: Demonstrates a sample implementation of a task whose progress can be tracked utilizing `NSProgress`.
-
+//: > A parent progress _may_ be _implicitly_ given by a call-site through using _thread local storage_. This example does not use thread local storage.
 
 //: ## Class Task
-//: In order to track progress of a task, we require a task to expose its "progress" via a property, `progress`.
-//:
-//: A parent progress _may_ be _implicitly_ given by a call-site through using _thread local storage_. This example
-//: does not use thread local storage, instead it requires the init function to provide additional parameters where
-//: the parent progress and its "pending unit count" value can be passed to the task instance.
-//: > Note: Here, the parameter `workUnits` in the `init` function just defines the _number of steps_ this task reports its progress. It does not have an effect on the progress at all. A real task implementation would use other means to report progress resulting in a reasonable number of steps. 
+//: In order to track progress of a task, we require a task to expose its "progress" via a property, `progress`. At any time, the progress's property `fractionCompleted` reflects the current progress of this task. `fractionCompleted` will range from 0.0 to 1.0.
+
+//: > Here, the init function of the task takes a parameter `workUnits`. This just defines the _number of steps_ this task reports its progress. Since this sample task uses a fixed duration per step, the parameter `workUnit` happens to define the duration this task takes to finish. A real task implementation would use other means when and how often it reports progress to its progress instance.
 class Task {
     private var timer: FutureLib.Timer!
     private let ec = GCDAsyncExecutionContext(dispatch_queue_create("task sync_queue", DISPATCH_QUEUE_SERIAL))
@@ -76,8 +73,7 @@ class ProgressObserver: NSObject {
 //: ## Objective
 //: Track the process of three tasks which execute sequentially
 
-
-//: Create three tasks and - for the sake of the sample - each take the same duration to finish:
+//: First, create three tasks in suspended state and - for the sake of the sample - each should take the same duration to finish:
 let task1 = Task(workUnits: 100)
 let task2 = Task(workUnits: 100)
 let task3 = Task(workUnits: 100)
@@ -86,23 +82,19 @@ let task3 = Task(workUnits: 100)
 let weight1: Int64 = 50
 let weight2: Int64 = 30
 let weight3: Int64 = 20
-//: Since a client does not really know the exact durations a tasks take to complete a priori, it should be clear that the weight values are just informed guesses. Ideally,
+//: So, what we are sayuing here is, `task1` takes 50 out of 100, `task2` takes 30 out of 100 and `task3` takes 20 out of 100 unit counts. The sum equals 100 unit count.
+//: Since a client does not really know the exact duration a task takes to complete a priori, it should be clear that the weight values are just _informed guesses_.
 
-
-//: Then, create the "parent progress", tracking the progress form task1, taks2 and task3 whose total amount of units equals the sum of the weight for each task:
+//: Then, create the "parent progress", tracking the sum of the progress from task1, task2 and task3 whose total amount of units equals the sum of the weight for each task:
 let parentProgress = NSProgress(totalUnitCount: weight1 + weight2 + weight3)
-//: Here, task1 takes 50 out of 100, task3 takes 30 out of 100 and task3 takes 20 out of 100 unit counts. The sum equals 100 unit count.
 
 //: Next, assign the parent progress the three "child progresses". This yields a parent progress which tracks the sequential execution of the three tasks:
 parentProgress.addChild(task1.progress, withPendingUnitCount: weight1)
 parentProgress.addChild(task2.progress, withPendingUnitCount: weight2)
 parentProgress.addChild(task3.progress, withPendingUnitCount: weight3)
 
-
-
-
 //: Add a Live view
-//: > Note: Open the Assistan View "Timeline" pane in order to view the progress indicator!
+//: > Open the Assistan View "Timeline" pane in order to view the progress indicator!
 let frame = CGRect(x: 0, y: 0, width: 200, height: 50)
 let containerView = NSView(frame: frame)
 let progressView = NSProgressIndicator(frame: frame)
@@ -117,7 +109,7 @@ XCPlaygroundPage.currentPage.liveView = containerView
 progressView.startAnimation(nil)
 
 //: Now, the parent progress' `fractionCompleted` property may be bound to an UI element displaying the progress for the three tasks.
-//: Create the observer, which prints the the current progress to the console:
+//: Create the observer, which updates the property `doubleValue` for the progress indicator:
 let observer = ProgressObserver(progress: parentProgress, handler: { value -> () in
     dispatch_async(dispatch_get_main_queue()) {
         //print(value)
@@ -127,8 +119,7 @@ let observer = ProgressObserver(progress: parentProgress, handler: { value -> ()
 })
 
 
-
-//: Start the task in sequence:
+//: Finally, start the tasks in sequence:
 task1.resume().flatMap { _ -> Future<Int64> in
     print("task 1 finished")
     return task2.resume().flatMap { _ -> Future<Int64> in
@@ -140,7 +131,6 @@ task1.resume().flatMap { _ -> Future<Int64> in
         progressView.stopAnimation(nil)
         print("task 3 finished")
     }
-    
 }
 
 
